9342809c237eaeebeae0495485958e029372158c
eslam69 <eslaamkhaled64@gmail.com>
2025-02-14 23:31:25 +0200

    Refactor header to use dynamic website title and update RSS feed title

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { myName } from './index.astro';
+import { WebsiteTitle } from "../pages/index.astro";
 
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: `${myName}'s Blog`,
+    title: `${WebsiteTitle}`,
     description: 'Thoughts, tutorials, and insights about technology and software development.',
     site: context.site,
     items: blog.map((post) => ({
